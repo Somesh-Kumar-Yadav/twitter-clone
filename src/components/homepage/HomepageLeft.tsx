87e3da7ecc52ee144b1e2a873b51fc7501712cff
Somesh <somesh_nj2_076@masai.school>
2021-10-06 22:43:51 +0530

    working on homepage

--- src/components/homepage/HomepageLeft.tsx
+++ src/components/homepage/HomepageLeft.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import styles from "../../styles/Homepage.module.css"
+import { myProfile } from '../../utils/myProfile'
 
 export const HomepageLeft = ():JSX.Element => {
     return <div className={styles.homepage_left_cont}>
-        <div>
+        <div className={styles.homepage_left_cont_top}>
             <ul>
-                <li>
+                <li className={styles.homepage_left_cont_top_top}>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
                         <g>
                             <path d="M23.643 4.937c-.835.37-1.732.62-2.675.733.962-.576 1.7-1.49 2.048-2.578-.9.534-1.897.922-2.958 1.13-.85-.904-2.06-1.47-3.4-1.47-2.572 0-4.658 2.086-4.658 4.66 0 .364.042.718.12 1.06-3.873-.195-7.304-2.05-9.602-4.868-.4.69-.63 1.49-.63 2.342 0 1.616.823 3.043 2.072 3.878-.764-.025-1.482-.234-2.11-.583v.06c0 2.257 1.605 4.14 3.737 4.568-.392.106-.803.162-1.227.162-.3 0-.593-.028-.877-.082.593 1.85 2.313 3.198 4.352 3.234-1.595 1.25-3.604 1.995-5.786 1.995-.376 0-.747-.022-1.112-.065 2.062 1.323 4.51 2.093 7.14 2.093 8.57 0 13.255-7.098 13.255-13.254 0-.2-.005-.402-.014-.602.91-.658 1.7-1.477 2.323-2.41z">
@@ -20,14 +21,16 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Home</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
                         <g>
-                            <path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z">
+                            <path d="M21 7.337h-3.93l.372-4.272c.036-.412-.27-.775-.682-.812-.417-.03-.776.27-.812.683l-.383 4.4h-6.32l.37-4.27c.037-.413-.27-.776-.68-.813-.42-.03-.777.27-.813.683l-.382 4.4H3.782c-.414 0-.75.337-.75.75s.336.75.75.75H7.61l-.55 6.327H3c-.414 0-.75.336-.75.75s.336.75.75.75h3.93l-.372 4.272c-.036.412.27.775.682.812l.066.003c.385 0 .712-.295.746-.686l.383-4.4h6.32l-.37 4.27c-.036.413.27.776.682.813l.066.003c.385 0 .712-.295.746-.686l.382-4.4h3.957c.413 0 .75-.337.75-.75s-.337-.75-.75-.75H16.39l.55-6.327H21c.414 0 .75-.336.75-.75s-.336-.75-.75-.75zm-6.115 7.826h-6.32l.55-6.326h6.32l-.55 6.326z">
                             </path>
                         </g>
                     </svg>
+                    <span>Explore</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -36,6 +39,7 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Notifications</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -44,6 +48,7 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Messages</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -52,6 +57,7 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Bookmarks</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -60,6 +66,7 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Lists</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -68,6 +75,7 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>Profile</span>
                 </li>
                 <li>
                     <svg viewBox="0 0 24 24" aria-hidden="true">
@@ -79,15 +87,16 @@ export const HomepageLeft = ():JSX.Element => {
                             </path>
                         </g>
                     </svg>
+                    <span>More</span>
                 </li>
             </ul>
         </div>
-        <div>
+        <div  className={styles.homepage_left_cont_mid}>
             <button>Tweet</button>
         </div>
-        <div>
+        <div  className={styles.homepage_left_cont_bottom}>
             <div>
-                <span><img src="" alt=""/></span>
+                <span><img src={myProfile.img} alt=""/></span>
                 <span>
                     <p><strong>Somesh Kumar Yadav</strong></p>
                     <p>@SomeshKumarYa12</p>
